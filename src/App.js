b2e4045c21d62948a3d4cf8593ac3bf998bee564
Sandeep Kumar <sandeep15045@gmail.com>
2023-03-29 11:21:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+import Products from './components/Products';
+
+const App = () => {
+  const products = [
+    {
+      id: 'p1',
+      title: 'Nirma',
+      amount: 100,
+      date: new Date(2021, 8, 10),
+    },
+    { 
+      id: 'p2', 
+      title: 'Sirf Excel', 
+      amount: 200, 
+      date: new Date(2021, 2, 2) },
+    {
+      id: 'p3',
+      title: 'Tide',
+      amount: 130,
+      date: new Date(2021, 12, 28),
+    },
+    {
+      id: 'p4',
+      title: 'Maggi',
+      amount: 450,
+      date: new Date(2021, 5, 5),
+    },
+  ];
+
+  return (
+    <div>
+      <p>hi </p>
+      <Products items={products} title={products[3].title} />
+    </div>
+  );
+}
+
+export default App;
